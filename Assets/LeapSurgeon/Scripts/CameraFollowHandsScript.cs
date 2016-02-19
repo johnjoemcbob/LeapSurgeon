56f5ea3c8fc078460a5b8908ccbd04dee061ea82
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-02-19 17:18:22 +0000

    Initial Commit
    Leap Motion Plugin
    Unity Base Effects
    Scene Start

---
+++ Assets/LeapSurgeon/Scripts/CameraFollowHandsScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Chooses the mid point between both hands and smooths the camera to focus there
+// Matthew Cormack @johnjoemcbob
+// 19/02/16 - 17:16
+
+public class CameraFollowHandsScript : MonoBehaviour
+{
+	void Update()
+	{
+
+	}
+}
