7b43804cb7bca4451b5cf4a8a6ea1adba83ea3e9
johnjoemcbob <mcormack1994@hotmail.co.uk>
2016-02-19 19:42:08 +0000

    Camera Movement, Pinching, Models
    Camera now follows leap hands smoothly
    Changes to internal leap utils for grabbing objects

--- Assets/LeapSurgeon/Scripts/CameraFollowHandsScript.cs
+++ Assets/LeapSurgeon/Scripts/CameraFollowHandsScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Leap;
 
 // Chooses the mid point between both hands and smooths the camera to focus there
 // Matthew Cormack @johnjoemcbob
@@ -7,8 +8,51 @@ using System.Collections;
 
 public class CameraFollowHandsScript : MonoBehaviour
 {
+	public float Influence = 0.5f;
+    public GameObject Hand_Left;
+	public GameObject Hand_Right;
+
+	private Vector3 BasePosition;
+
+	void Start()
+	{
+		BasePosition = transform.position;
+	}
+
 	void Update()
 	{
+		// Position
+		Vector3 pos = BasePosition + ( HandInfluence( Hand_Left ) * Influence ) + ( HandInfluence( Hand_Right ) * Influence );
+		float mag = Vector3.Distance( transform.position, pos ) * 1;
+		transform.position = Vector3.Lerp( transform.position, pos, Time.deltaTime * mag );
 
+		// Rotation
+		Vector3 handcenter = HandInfluence( Hand_Left ) + HandInfluence( Hand_Right );
+        Vector3 lookat = -( transform.position - handcenter );
+		{
+            lookat.Normalize();
+		}
+        Quaternion rot = Quaternion.LookRotation( lookat );
+		transform.rotation = Quaternion.Lerp( transform.rotation, rot, Time.deltaTime );
 	}
+
+	private Vector3 HandInfluence( GameObject hand )
+	{
+		Vector3 influence = Vector3.zero;
+		{
+			Hand leaphand = (Hand) hand.GetComponent<CapsuleHand>().GetLeapHand();
+            if ( hand.activeInHierarchy && ( leaphand != null ) )
+			{
+				// Don't influence the camera if they are offscreen
+				Vector pos = leaphand.PalmPosition;
+				if ( ( pos.y > -0.5f ) && ( pos.z > -1.5f ) )
+				{
+					influence.x = pos.x;
+					influence.y = pos.y;
+					influence.z = pos.z;
+				}
+			}
+		}
+        return influence;
+    }
 }
